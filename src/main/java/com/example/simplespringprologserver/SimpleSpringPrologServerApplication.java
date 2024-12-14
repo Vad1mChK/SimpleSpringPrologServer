1d7490e3aee26119d5e3b8f59892ea1ad680598e
Vadim Chaykin <snusmumrike2@gmail.com>
2024-12-14 17:16:37 +0300

    .

---
+++ src/main/java/com/example/simplespringprologserver/SimpleSpringPrologServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplespringprologserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringPrologServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringPrologServerApplication.class, args);
+    }
+
+}
